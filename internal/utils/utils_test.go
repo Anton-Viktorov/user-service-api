9f0c1967855b317ce20fed6ebc3b0016d1da0ecc
Anton Viktorov <anthony.viktorov@gmail.com>
2022-04-03 17:48:03 +0300

    fix utils tests

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -42,7 +42,7 @@ func TestSplitSlice(t *testing.T) {
 	}
 
 	for _, data := range datasets {
-		result := SplitSlice(data.InputSlice, data.ChunkSize)
+		result, _ := SplitSlice(data.InputSlice, data.ChunkSize)
 		require.Equalf(
 			t, data.OutputSlice, result, "Test failed. Expected: %v, Given %v", data.OutputSlice, result,
 		)
@@ -52,18 +52,18 @@ func TestSplitSlice(t *testing.T) {
 func TestReverseKey(t *testing.T) {
 
 	type Dataset struct {
-		InputMap  map[int64]int64
-		OutputMap map[int64]int64
+		InputMap  map[int64]string
+		OutputMap map[string]int64
 	}
 
 	datasets := []Dataset{
 		{
-			InputMap:  map[int64]int64{1: 12, 2: 22, 3: 32},
-			OutputMap: map[int64]int64{12: 1, 22: 2, 32: 3},
+			InputMap:  map[int64]string{1: "one", 2: "two", 3: "three"},
+			OutputMap: map[string]int64{"one": 1, "two": 2, "three": 3},
 		},
 		{
-			InputMap:  map[int64]int64{},
-			OutputMap: map[int64]int64{},
+			InputMap:  map[int64]string{},
+			OutputMap: map[string]int64{},
 		},
 		{
 			InputMap:  nil,
@@ -72,43 +72,54 @@ func TestReverseKey(t *testing.T) {
 	}
 
 	for _, data := range datasets {
-		result := ReverseKey(data.InputMap)
+		result, _ := ReverseKey(data.InputMap)
 		require.Equalf(t, data.OutputMap, result, "Test failed. Expected: %v, Given %v", data.OutputMap, result)
 	}
 }
 
-func TestPanicReverseKey(t *testing.T) {
-	wrongMap := map[int64]int64{1: 10, 2: 10}
-	require.Panicsf(t, func() { ReverseKey(wrongMap) }, "Panic does not raise")
+func TestErrorReverseKey(t *testing.T) {
+	wrongMap := map[int64]string{1: "one", 2: "one"}
+	_, err := ReverseKey(wrongMap)
+	require.Error(t, err, "Error does not raise")
 }
 
 func TestFilterSlice(t *testing.T) {
 	type Dataset struct {
 		InputSlice  []int64
+		FilterSlice []int64
 		OutputSlice []int64
 	}
 
 	datasets := []Dataset{
 		{
 			InputSlice:  []int64{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			FilterSlice: []int64{1, 3, 5, 7, 9},
 			OutputSlice: []int64{2, 4, 6, 8},
 		},
 		{
 			InputSlice:  []int64{1, 1, 3, 3, 4, 4},
+			FilterSlice: []int64{1, 3, 5, 7, 9},
 			OutputSlice: []int64{4, 4},
 		},
 		{
-			InputSlice:  []int64{},
-			OutputSlice: []int64{},
+			InputSlice:  []int64{1, 2, 3},
+			FilterSlice: []int64{},
+			OutputSlice: []int64{1, 2, 3},
+		},
+		{
+			InputSlice:  []int64{1, 2, 3},
+			FilterSlice: nil,
+			OutputSlice: []int64{1, 2, 3},
 		},
 		{
 			InputSlice:  nil,
+			FilterSlice: nil,
 			OutputSlice: nil,
 		},
 	}
 
 	for _, data := range datasets {
-		result := FilterSlice(data.InputSlice)
+		result, _ := FilterSlice(data.InputSlice, data.FilterSlice)
 		require.Equalf(
 			t, data.OutputSlice, result, "Test failed. Expected: %v, Given %v", data.OutputSlice, result,
 		)
